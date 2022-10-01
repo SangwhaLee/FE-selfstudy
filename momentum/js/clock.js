27c4d923d3e33c31c196f7c2392d92afe4605099
SangwhaLee <sanghwa1103@gmail.com>
2022-10-01 15:27:12 +0900

    to clock

---
+++ momentum/js/clock.js
@@ -0,0 +1,13 @@
+const clock = document.querySelector("h2#clock");
+
+function getClock(){
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
+
