27d1cc1e8b02a8ff761e2e8d01f2eee5834869b2
SangwhaLee <sanghwa1103@gmail.com>
2022-10-07 18:02:15 +0900

    main focus done

--- new_momentum/js/main_focus.js
+++ new_momentum/js/main_focus.js
@@ -1,32 +1,32 @@
 
 
-const focusForm = document.getElementById("focus-today")
-const focusInput = focusForm.querySelector("#focus-input")
-const MainFocus = document
+const todayFocus = document.getElementById("focus-today");
+const focusInput = todayFocus.querySelector("#focus-today input");
+const MainFocus = document.querySelector("#FocusToday");
 
-const HIDDEN_CLASSNAME = "hidden";
-const FOCUS_KEY = "focus_today";
-
-const savedFocus = localStorage.getItem(FOCUS_KEY);
+const HIDDEN= "hidden";
+const FOCUS_KEY = "focustoday";
 
 function onFocusSubmit(event){
+  console.log(event);
   event.preventDefault();
-  focusForm.classList.add(HIDDEN_CLASSNAME);
+  todayFocus.classList.add (HIDDEN);
   const mainfocus = focusInput.value;
   localStorage.setItem(FOCUS_KEY, mainfocus);
   paintFocus(mainfocus);
 }
 
 function paintFocus(mainfocus){
-  MainFocus.classList.remove(HIDDEN_CLASSNAME);
+  MainFocus.classList.remove (HIDDEN);
   MainFocus.innerText = mainfocus;
 }
 
+const savedFocus = localStorage.getItem(FOCUS_KEY);
+
 if(savedFocus === null){
-  focusForm.classList.remove(HIDDEN_CLASSNAME);
-  focusForm.addEventListener("submit", onFocusSubmit);
+  todayFocus.addEventListener("submit", onFocusSubmit);
 }
 else{
-  focusForm.classList.add(HIDDEN_CLASSNAME);
+  todayFocus.classList.add (HIDDEN);
   paintFocus(savedFocus);
 }
