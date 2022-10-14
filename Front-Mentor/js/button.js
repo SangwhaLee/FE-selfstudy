07059e6d5b19582bc32b0bd2a5450ed83cf09753
SangwhaLee <sanghwa1103@gmail.com>
2022-10-14 10:51:37 +0900

    modified 1014

--- Front-Mentor/js/button.js
+++ Front-Mentor/js/button.js
@@ -8,16 +8,17 @@ function checkOneBox(event){
   }
 }
 
-// function nextStage(event){
-
-// }
+function nextStage(event){
+  const resultbox = document.getElementById("resultbox");
+  resultbox.classList.remove("hidden");
+}
 
 const circleButton1 = document.getElementById("button1");
 const circleButton2 = document.getElementById("button2");
 const circleButton3 = document.getElementById("button3");
 const circleButton4 = document.getElementById("button4");
 const circleButton5 = document.getElementById("button5");
-const submitButton = document.getElementById("submitbutton")
+const submitButton = document.querySelector("div .submitbutton");
 
 circleButton1.addEventListener("click", checkOneBox);
 circleButton2.addEventListener("click", checkOneBox);
@@ -25,5 +26,5 @@ circleButton3.addEventListener("click", checkOneBox);
 circleButton4.addEventListener("click", checkOneBox);
 circleButton5.addEventListener("click", checkOneBox);
 
-// submitButton.addEventListener("click", )
+submitButton.addEventListener("click", nextStage);
 
