d460c024b4a792a3bbc9b0f96863ecfb7144b334
SangwhaLee <sanghwa1103@gmail.com>
2022-10-21 17:54:59 +0900

    1021 racing car

---
+++ RacingCar/src/racing.js
@@ -0,0 +1,10 @@
+class Car {
+  constructor (name) {
+    this.name = name;
+  }
+}
+
+const carNameInput = document.getElementById("car-names-input");
+const carNameForm = document.getElementById("car-names-form");
+const racingCountForm = document.getElementById("racing-count-form");
+
